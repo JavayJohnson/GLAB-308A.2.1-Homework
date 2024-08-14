3a1a5440d1c39f5118777d0053327157c895b0e7
javay johnson <javayjohnson@gmail.com>
2024-08-13 21:05:55 -0400

    Final Push- something is wrong with this file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 
 // Part 1: Humble Beginnings
-// Let’s model a simple adventurer with basic properties such as health and an inventory. 
-// We will call the adventurer “Robin.”
 
 const adventurer = {
 name: "Robin",
@@ -9,7 +7,14 @@ health: 10,
 inventory: ["sword", "potion", "artifact"]
 }
 
-// Nested arrays are useful, but so are nested objects. Let’s give Robin a companion to travel with – a furry friend they call “Leo.”
+function inventoryLog() {
+    adventurer.inventory.forEach((item) => {
+        console.log(item)
+    })
+}
+
+inventoryLog()
+
 
 const adventurer = {
     name: "Robin",
@@ -21,14 +26,18 @@ const adventurer = {
     }
     }
 
-    // Now we have created an array within an object within an object within an object, but that is not all we can do! Objects can also have their own functions, called methods, which define specific actions that object can take.
-    // Give Robin the following method:
+    adventurer.companion = {
+        name: 'Frank',
+        type: 'Flea',
+        belongings: ['small hat', 'sunglasses']
+
+    }
 
     const adventurer = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"],
-    companion: ...
+    companion: ...,
     roll (mod = 0) {
     const result = Math.floor(Math.random() * 20) + 1 + mod;
     console.log(`${this.name} rolled a ${result}.`)
@@ -37,10 +46,6 @@ const adventurer = {
     
 
     // Part 2: Class Fantasy
-    // Let’s take a moment to analyze the data above. What are the common features of each object?
-    // When creating classes, begin by searching for the simplest form your data takes. Remember, you can add specificity later by extending the classes.
-    // Start with a Character class, which will define generic character entities. Robin and their companions all have a name, so the Character class should definitely include name as one of its properties. At this stage, we will also make the decision that every character should have health (which we will standardize to a maximum of 100, and an inventory (even if the inventory is empty).
-    // Here is what the basic Character class looks like so far, including a constructor function that allows us to create new characters with whatever name we would like:
     
     class Character {
       constructor (name) {
@@ -50,53 +55,41 @@ const adventurer = {
       }
     }
 
-
-    
-    //  Every character should also be able to make rolls. Add the roll method to the Character class.
-    // Now, we can re-create Robin using the Character class!
-
     const robin = new Character("Robin");
     robin.inventory = ["sword", "potion", "artifact"];
     robin.companion = new Character("Leo");
     robin.companion.type = "Cat";
     robin.companion.companion = new Character("Frank");
     robin.companion.companion.type = "Flea";
-    robin.companion.companion.inventory = ["small hat", "sunglasses"]
-
-
-
+    robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 
 
     // Part 3: Class Features
-    // When extending a class, the “child” class inherits all properties of its parents. This means that we do not need to account for the name, health, inventory, or roll method of Character children classes.
-    // Let’s begin by creating an Adventurer class. What attributes might be specific to an adventure, but that not all characters have? Take a look at our example below, and expand upon it with your own properties and methods.
+
     
     class Adventurer extends Character {
       constructor (name, role) {
         super(name);
-        // Adventurers have specialized roles.
-        this.role = role;
-        // Every adventurer starts with a bed and 50 gold coins.
         this.inventory.push("bedroll", "50 gold coins");
       }
-      // Adventurers have the ability to scout ahead of them.
       scout () {
         console.log(`${this.name} is scouting ahead...`);
         super.roll();
-      }
-    }
-
+        }
+ }
 
 class Companion extends Companion Adventurer {
-    constructor(name,type){
-        super(name);
-    }
+    constructor(companionName,type) {
+        super();
+        this.companionName = companionName
+        this.type =type;
 
+    }
 }
 
 cheer() {
-    console.log('You are AWESOME ${super.name}');
+    console.log(`You are AWESOME ${super.name}`);
 }
 
 const Leo = new Companion ("Leo","Cat");
@@ -113,9 +106,33 @@ Leo.cheer()
 // Are there other static properties or methods that make sense to add to these classes?
 
 
+static = ROLES + ["Fighter","Healer","Wizard", "Fire thrower"];
+contructor (name, role){
+    super(name)
 
+    if (role +++ "Fighter" || role +++ "Healer" || role ==="Wizard" || role ==="Fire thrower")
+        this.role =role;
 
+}
 
+const MAX_HEALTH = {
+    health: 100,
+}
+class Character{
+    static overallHealth(health) {
+        console.log(health);
+    }
+}
+
+class Character {
+    static MAX_HEALTH = {
+        health: 100,
+    };
+
+    static overallHealth() {
+        console.log(this.MAX_HEALTH.health);
+    }
+}
 
 
 
